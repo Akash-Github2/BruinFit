6044558d722bfb361e63ee451bcb6d2247ba113e
Kareena Kullar <kareenakullar@g.ucla.edu>
2022-03-02 00:05:36 -0800

    styled nav bar

--- bruin-fit/src/App.js
+++ bruin-fit/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
-import './App.css'
+import './App.css';
+import { Routes, Router, Route } from 'react-router-dom';
+import Home from './Home'
+import Food from './Food'
+
 
 
 
 
 function App() {
   return (
-    <div className='App'>
+       <div className='App'>
+    
+    {/* <Router>  */}
       <Navbar />
-    </div>
+      {/* <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/food' component={Food} />
+        <Route path='/fitness' component={Fitness} />
+        <Route path='/friends' component={Friends} />
+        <Route path='/signup' component={SignUp} />
+      </Routes>
+    </Router> */}
+      
+
+        </div>
+      
   );
 }
 export default App;
