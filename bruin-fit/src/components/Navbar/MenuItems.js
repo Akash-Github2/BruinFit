f39f1784229db89ecc34099eb68bc573605f2159
Kareena Kullar <kareenakullar@g.ucla.edu>
2022-03-02 00:10:50 -0800

    merged

--- bruin-fit/src/components/Navbar/MenuItems.js
+++ bruin-fit/src/components/Navbar/MenuItems.js
@@ -1,47 +1,27 @@
 export const MenuItems = [
     {
         title: 'Home',
-<<<<<<< HEAD
         url: '/',
-=======
-        url: '#',
->>>>>>> d20af01fbedab1df9ff7974add0845d59755a1ce
         cName: 'nav-links'
     },
     {
         title: 'Food',
-<<<<<<< HEAD
         url: '/food',
-=======
-        url: '#',
->>>>>>> d20af01fbedab1df9ff7974add0845d59755a1ce
         cName: 'nav-links'
     },
     {
         title: 'Fitness',
-<<<<<<< HEAD
         url: '/fitness',
-=======
-        url: '#',
->>>>>>> d20af01fbedab1df9ff7974add0845d59755a1ce
         cName: 'nav-links'
     },
     {
         title: 'Friends',
-<<<<<<< HEAD
         url: '/friends',
-=======
-        url: '#',
->>>>>>> d20af01fbedab1df9ff7974add0845d59755a1ce
         cName: 'nav-links'
     },
     {
         title: 'Sign up',
-<<<<<<< HEAD
         url: '/signup',
-=======
-        url: '#',
->>>>>>> d20af01fbedab1df9ff7974add0845d59755a1ce
         cName: 'nav-links-mobile'
     },
 
