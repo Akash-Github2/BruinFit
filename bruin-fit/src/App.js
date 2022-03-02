3164a9a6187dc4a0dfe222d40d42c660a0a43de2
Akash Pulinthanathu <akashpulinthanathu@gmail.com>
2022-03-01 23:36:57 -0800

    Added firebase

--- bruin-fit/src/App.js
+++ bruin-fit/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import Navbar from "./components/Navbar/Navbar";
+// import Navbar from "./components/Navbar/Navbar";
 import './App.css'
-
-
-
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import {
+//   Home,
+//   Food,
+//   Fitness,
+//   Friends
+// } from "./components";
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className='App'>
-      <Navbar />
+      <SignUp />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/fitness" element={<Fitness />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/sign-up" element={<Contact />} />
+      </Routes> */}
     </div>
   );
 }
